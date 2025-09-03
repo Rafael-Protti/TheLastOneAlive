a07f39e828f41490d08e318228ddd6bef1bacea1
Andrezinhos <andremiguel2magon@gmail.com>
2025-09-03 17:35:51 -0300

    script do player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        if (move == 1)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            estaOlhandoDireita = true;
+        }
+        if (move == -1)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            estaOlhandoDireita = false;
+        }
+
+        rigib.AddForce(new Vector2(move * velo, 0));
+
+        move = move * velo * Time.deltaTime;
+        rigib.linearVelocityX = Mathf.Clamp(rigib.linearVelocityX, -velomax, velomax);
+    }
+}
