f55876352c350adbb5159b71ba9e6dfbe4437d3d
Andrezinhos <andremiguel2magon@gmail.com>
2025-09-04 16:38:44 -0300

    animação de ataque ok

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@
 public class Player : MonoBehaviour
 {
     Rigidbody2D rigib;
+    Animator anima;
 
     public float velo = 10;
     public float velomax = 5;
@@ -13,15 +14,39 @@ public class Player : MonoBehaviour
     void Start()
     {
         rigib = transform.GetComponent<Rigidbody2D>();
+        anima = transform.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool ataque = Input.GetKey(KeyCode.RightShift);
+
+        if (ataque && podePular == true)
+        {
+            anima.SetBool("estaAtacando", true);
+        }
+        else
+        {
+            anima.SetBool("estaAtacando", false);
+        }
     }
 
     void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            podePular = true;
+            anima.SetBool("estaPulando", false);
+        }    
+    }
+
+    void MovePlayer()
     {
         //movimento do player
         float move = Input.GetAxisRaw("Horizontal");
@@ -43,21 +68,23 @@ public class Player : MonoBehaviour
         move = move * velo * Time.deltaTime;
         rigib.linearVelocityX = Mathf.Clamp(rigib.linearVelocityX, -velomax, velomax);
 
+        //animação de corrida
+        if (rigib.linearVelocityX < 0.1f && rigib.linearVelocityX > -0.1f)
+        {
+            anima.SetBool("estaCorrendo", false);
+        }
+        else
+        {
+            anima.SetBool("estaCorrendo", true);
+        }
+
         //pulo do player
         bool pulo = Input.GetAxisRaw("Jump") > 0;
         if (pulo == true && podePular == true)
         {
+            anima.SetBool("estaPulando", true);
             rigib.AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
             podePular = false;
         }
     }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            podePular = true;
-        }    
-    }
-
 }
