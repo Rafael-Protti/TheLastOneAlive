6f7a2650a46431fa18b63151090f43eabb554752
Andrezinhos <andremiguel2magon@gmail.com>
2025-09-04 13:53:05 -0300

    script de movimento ok

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,36 +2,31 @@
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rigib;
+
+    public float velo = 10;
+    public float velomax = 5;
+
     void Start()
     {
-        
+        rigib = transform.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        bool pulo = Input.GetAxisRaw("Jump") > 0;
     }
 
     void FixedUpdate()
     {
         float move = Input.GetAxisRaw("Horizontal");
 
-        if (move == 1)
-        {
-            transform.eulerAngles = new Vector2(0, 0);
-            estaOlhandoDireita = true;
-        }
-        if (move == -1)
-        {
-            transform.eulerAngles = new Vector2(0, 180);
-            estaOlhandoDireita = false;
-        }
-
         rigib.AddForce(new Vector2(move * velo, 0));
 
         move = move * velo * Time.deltaTime;
         rigib.linearVelocityX = Mathf.Clamp(rigib.linearVelocityX, -velomax, velomax);
     }
+
 }
