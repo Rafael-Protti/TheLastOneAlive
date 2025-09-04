7b18b589596746e33fef96f04e6ec3f11efa6af1
Andrezinhos <andremiguel2magon@gmail.com>
2025-09-04 14:09:26 -0300

    pulo do player ok

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 
     public float velo = 10;
     public float velomax = 5;
+    public float forcaPulo = 10;
+    bool podePular = true; 
 
     void Start()
     {
@@ -16,17 +18,33 @@ public class Player : MonoBehaviour
     void Update()
     {
         
-        bool pulo = Input.GetAxisRaw("Jump") > 0;
     }
 
     void FixedUpdate()
     {
+        //movimento do player
         float move = Input.GetAxisRaw("Horizontal");
 
         rigib.AddForce(new Vector2(move * velo, 0));
 
         move = move * velo * Time.deltaTime;
         rigib.linearVelocityX = Mathf.Clamp(rigib.linearVelocityX, -velomax, velomax);
+
+        //pulo do player
+        bool pulo = Input.GetAxisRaw("Jump") > 0;
+        if (pulo == true && podePular == true)
+        {
+            rigib.AddForce(new Vector2(0, forcaPulo), ForceMode2D.Impulse);
+            podePular = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            podePular = true;
+        }    
     }
 
 }
